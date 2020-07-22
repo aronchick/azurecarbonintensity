eb87493e1169fb91edbe781ea3520f0d6325987f
David Aronchick <aronchick@gmail.com>
2020-07-22 23:17:02 +0000

    API now works

--- get_carbon_by_vm/calculator.py
+++ get_carbon_by_vm/calculator.py
@@ -8,10 +8,10 @@ sys.path.append(str(Path.cwd().parent))
 
 from get_carbon_by_vm.hw_lookup import calculate_co2_for_vm
 from get_carbon_by_vm.datacenter import datacenter_multiplier
-from get_carbon_by_vm.webservices import co2_load_by_time_and_region
+from get_carbon_by_vm.webservices import co2_per_watt_by_time_and_region
 
 def calculate_co2(vm_type, datacenter_region,  number_of_cores, evaluation_time=datetime.datetime.now()):
     vm_co2 = calculate_co2_for_vm(vm_type)
     dc_multiplier_value = datacenter_multiplier(datacenter_region)
-    current_co2_load = co2_load_by_time_and_region(datacenter_region, evaluation_time)
+    current_co2_load = co2_per_watt_by_time_and_region(datacenter_region, evaluation_time)
     return vm_co2 * dc_multiplier_value * number_of_cores * current_co2_load
--- get_carbon_by_vm/webservices.py
+++ get_carbon_by_vm/webservices.py
@@ -1,42 +1,59 @@
+from random import randrange
 import sys
 from pathlib import Path
 
+import logging
+import os
+import azure.functions as func
+    
 sys.path.append(str(Path.cwd()))
 sys.path.append(str(Path.cwd().parent))
 
 import get_carbon_by_vm.region_list_and_multiplier
+from get_carbon_by_vm.watttime_api import login, index
 
-def co2_load_by_time_and_region(datacenter_region, evaluation_time):
+import yaml as YAML
+
+def co2_per_watt_by_time_and_region(datacenter_region, evaluation_time):
     datacenter_multiplier = get_carbon_by_vm.region_list_and_multiplier.list.get(datacenter_region,None)
-    get_co2_for_region_by_time()
+    carbon_dict = get_co2_for_region_by_time(datacenter_region, evaluation_time)
 
-    raise NotImplementedError("NYI")
+    # TODO: Remove mockup value below
+    # Get maximum CO2 by region for non-renewable
+    # max_co2 = get_maximum_co2_by_region(datacenter_region)
+    max_co2 = 1500
 
-def get_co2_for_region_by_time(time_to_evaluate):
-    # Replace below with actual evaluator (e.g. from watt time)
+    percent_renewable = carbon_dict['percent']
 
-    # For current: https://api2.watttime.org/v2/index/?latitude=42.372&longitude=72.519&style=all
-    #     from urllib2 import Request, urlopen
+    co2_per_watt = max_co2 * float(percent_renewable) / 100
+    return co2_per_watt
 
-    # headers = {
-    #   'Authorization': 'Bearer <Access_Token>',
-    #   'Accept': 'application/zip'
-    # }
-    # request = Request('https://api2.watttime.org/v2/historical/?ba=&version=', headers=headers)
-
-    # response_body = urlopen(request).read()
-    # print response_body
-
-
-    # For historical: 
-    #     
-    # from urllib2 import Request, urlopen
-
-    # headers = {
-    #   'Authorization': 'Bearer <Access_Token>'
-    # }
-    # request = Request('https://api2.watttime.org/v2/index/?latitude=&longitude=&style=', headers=headers)
-
-    # response_body = urlopen(request).read()
-    # print response_body
+def get_co2_for_region_by_time(region, time_to_evaluate):
+    """ Returns a % of how clean the region is.
     
+    Though it takes 'time_to_evaluate' and 'region', neither is currently used (just mocking up for region='CAISO_ZP26' and time=now)
+    """ 
+    # TODO: Support other times (e.g. for forecasting)
+
+    # Replace below with production evaluator (e.g. from watt time)
+    # https://github.com/WattTime/apiv2-example/blob/master/query_apiv2.py
+
+    username = os.environ["watttime_USERNAME"]
+    password = os.environ["watttime_PASSWORD"]
+
+    wt_token = login(username, password)
+
+    # TODO: Map from Azure region to BA region (a WattTime Concept)
+    BA = 'CAISO_ZP26'  # identify grid region
+
+    carbon_dict = index(wt_token, BA)
+
+    return carbon_dict
+
+
+    # "watttime_information": {
+    #   "USERNAME": "aronchick@gmail.com",
+    #   "PASSWORD": "73ad35ddc87843458a5a691f3d72dc5e",
+    #   "EMAIL": "aronchick@gmail.com",
+    #   "ORG": "None"
+    # }
